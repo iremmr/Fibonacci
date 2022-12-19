c01b318046c1234de69b6dfd0f2d936f01acd5ce
İrem Rabia Topal <117042843+iremmr@users.noreply.github.com>
2022-12-19 21:14:06 +0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 1, b = 1, d;
+        int c;
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Eleman sayısını giriniz:");
+        d = input.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+
+        for (int i = 1; i <= d - 2; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(c);
+
+        }
+
+    }
+}
